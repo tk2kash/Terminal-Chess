90b0904bee83ac2ef585424cb317f373ce0e6473
tk2kash <79985636+tk2kash@users.noreply.github.com>
2024-01-03 15:37:40 +0000

    Delete src directory

--- src/CheckInput.java
+++
@@ -1,9 +0,0 @@
-package chess;
-
-public class CheckInput {
-	
-	//This method requires your input
-	public boolean checkCoordinateValidity(String input){
-		return input.matches("([1-8][a-hA-H])");
-	}
-}
